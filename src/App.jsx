a028352199370e43f888b9655146b492d02a8488
Kang Liu <555836+cnkang@users.noreply.github.com>
2024-09-10 18:17:49 +0800

    lazyload large js files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react'; // Import React and useState hook
-import { jsPDF } from 'jspdf'; // Import jsPDF library for PDF generation
+import React, { useState, useEffect, Suspense } from 'react'; // Suspense is needed for dynamic imports
 import './App.css'; // Import CSS for styling
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
-/**
- * The main component of the MathGenie application.
- *
- * @return {JSX.Element} The rendered MathGenie application.
- */
+const SpeedInsights = React.lazy(() =>
+  import('@vercel/speed-insights/react').then((module) => ({ default: module.SpeedInsights }))
+);
+
 function App() {
   const [settings, setSettings] = useState({
-    operations: ['+'], // Allowed operations
-    numProblems: 10, // Number of problems to generate
-    numRange: [1, 10], // Range of numbers for operands
-    resultRange: [0, 100], // Acceptable range for results
-    numOperandsRange: [2, 3], // Range for the number of operands
-    allowNegative: false, // Whether to allow negative results
-    showAnswers: true, // Whether to show answers
-    fontSize: 12, // Font size for PDF
-    lineSpacing: 10, // Line spacing for PDF
-    paperSize: 'a4', // Paper size for PDF
+    operations: ['+'],
+    numProblems: 10,
+    numRange: [1, 10],
+    resultRange: [0, 100],
+    numOperandsRange: [2, 3],
+    allowNegative: false,
+    showAnswers: true,
+    fontSize: 12,
+    lineSpacing: 10,
+    paperSize: 'a4',
   });
   const [problems, setProblems] = useState([]);
 
@@ -29,13 +26,6 @@ function App() {
     'legal': 'legal',
   };
 
-  /**
-   * Calculates the result of an expression with given operands and operators.
-   *
-   * @param {Array<number>} operands - The array of operands.
-   * @param {Array<string>} operators - The array of operators.
-   * @return {number} The result of the expression.
-   */
   const calculateExpression = (operands, operators) => {
     let result = operands[0];
 
@@ -64,20 +54,8 @@ function App() {
     return result;
   };
 
-  /**
-   * Generates a math problem with random operands and operations within the specified ranges.
-   *
-   * @return {string} The generated math problem, or an empty string if no valid problem was generated in MAX_ATTEMPTS attempts.
-   */
   const generateProblem = () => {
     const crypto = window.crypto || window.msCrypto;
-    /**
-     * Generates a random number between the given minimum and maximum values.
-     *
-     * @param {number} min - The minimum value of the range.
-     * @param {number} max - The maximum value of the range.
-     * @return {number} The randomly generated number.
-     */
     const random = (min, max) => {
       const array = new Uint32Array(1);
       crypto.getRandomValues(array);
@@ -110,7 +88,6 @@ function App() {
             problem += ` ${operator} ${operands[index + 1]}`;
           });
 
-          // Replace '*' and '/' with visual symbols
           problem = problem.replace(/\*/g, '✖').replace(/\//g, '➗');
 
           return settings.showAnswers ? `${problem} = ${result}` : `${problem} = `;
@@ -118,14 +95,9 @@ function App() {
       }
     }
 
-    return ''; // Return an empty string if no valid problem was generated in MAX_ATTEMPTS
+    return ''; 
   };
 
-  /**
-   * Generates a set of problems based on the current settings and updates the state with the generated problems.
-   *
-   * @return {void} This function does not return anything.
-   */
   const generateProblems = () => {
     const generatedProblems = Array.from({ length: settings.numProblems }, () =>
       generateProblem()
@@ -133,56 +105,46 @@ function App() {
     setProblems(generatedProblems);
   };
 
-  /**
- * Downloads a PDF file containing the generated problems.
- *
- * This function creates a new instance of the jsPDF library and sets the
- * paper size and font size based on the settings. It then iterates over
- * the generated problems and adds each problem to the PDF document at a
- * specific position. Finally, it saves the PDF file with the name 'problems.pdf'.
- *
- * @return {void} This function does not return anything.
- */
   const downloadPdf = () => {
-    const doc = new jsPDF({
-      format: paperSizeOptions[settings.paperSize],
-    });
+    import('jspdf').then(({ default: jsPDF }) => {
+      const doc = new jsPDF({
+        format: paperSizeOptions[settings.paperSize],
+      });
 
-    doc.setFontSize(settings.fontSize);
-    const pageHeight = doc.internal.pageSize.getHeight();
-    const pageWidth = doc.internal.pageSize.getWidth();
+      doc.setFontSize(settings.fontSize);
+      const pageHeight = doc.internal.pageSize.getHeight();
+      const pageWidth = doc.internal.pageSize.getWidth();
 
-    const marginLeft = 10;
-    const marginTop = 10;
-    const lineSpacing = settings.lineSpacing;
-    const colWidth = (pageWidth - 3 * marginLeft) / 2;
+      const marginLeft = 10;
+      const marginTop = 10;
+      const lineSpacing = settings.lineSpacing;
+      const colWidth = (pageWidth - 3 * marginLeft) / 2;
 
-    let currYLeft = marginTop;
-    let currYRight = marginTop;
+      let currYLeft = marginTop;
+      let currYRight = marginTop;
 
-    problems.forEach((problem, index) => {
-      if (index % 2 === 0) {
-        // Left column
-        if (currYLeft + lineSpacing > pageHeight) {
-          doc.addPage();
-          currYLeft = marginTop;
-          currYRight = marginTop;
+      problems.forEach((problem, index) => {
+        if (index % 2 === 0) {
+          if (currYLeft + lineSpacing > pageHeight) {
+            doc.addPage();
+            currYLeft = marginTop;
+            currYRight = marginTop;
+          }
+          doc.text(problem.text, marginLeft, currYLeft);
+          currYLeft          += lineSpacing;
+        } else {
+          if (currYRight + lineSpacing > pageHeight) {
+            doc.addPage();
+            currYLeft = marginTop;
+            currYRight = marginTop;
+          }
+          doc.text(problem.text, marginLeft + colWidth + marginLeft, currYRight);
+          currYRight += lineSpacing;
         }
-        doc.text(problem.text, marginLeft, currYLeft);
-        currYLeft += lineSpacing;
-      } else {
-        // Right column
-        if (currYRight + lineSpacing > pageHeight) {
-          doc.addPage();
-          currYLeft = marginTop;
-          currYRight = marginTop;
-        }
-        doc.text(problem.text, marginLeft + colWidth + marginLeft, currYRight);
-        currYRight += lineSpacing;
-      }
-    });
+      });
 
-    doc.save('problems.pdf');
+      doc.save('problems.pdf');
+    });
   };
 
   useEffect(() => {
@@ -200,87 +162,27 @@ function App() {
     <div className="App">
       <h1>MathGenie</h1>
       <div className="container">
-        <div className="form-group operations-container">
-          <label htmlFor="operations">Operations:</label>
+        <div>
+          <label htmlFor="operations">Operations</label>
           <select
             id="operations"
             multiple
-            className="operations-select"
             value={settings.operations}
-            onChange={(e) => handleChange('operations', [...e.target.selectedOptions].map((option) => option.value))}
+            onChange={(e) =>
+              handleChange(
+                'operations',
+                Array.from(e.target.selectedOptions, (option) => option.value)
+              )
+            }
           >
             <option value="+">+</option>
             <option value="-">-</option>
-            <option value="*">✖</option>
-            <option value="/">➗</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
           </select>
         </div>
 
-        <div className="form-group">
-          <label htmlFor="numProblems">Number of Problems:</label>
-          <input
-            type="number"
-            id="numProblems"
-            value={settings.numProblems}
-            onChange={(e) => handleChange('numProblems', parseInt(e.target.value))}
-          />
-        </div>
-
-        <div className="form-group range-container">
-          <label htmlFor="numRange">Number Range:</label>
-          <input
-            type="number"
-            id="numRangeMin"
-            value={settings.numRange[0]}
-            onChange={(e) => handleChange('numRange', [parseInt(e.target.value), settings.numRange[1]])}
-            placeholder="Min"
-          />
-          <input
-            type="number"
-            id="numRangeMax"
-            value={settings.numRange[1]}
-            onChange={(e) => handleChange('numRange', [settings.numRange[0], parseInt(e.target.value)])}
-            placeholder="Max"
-          />
-        </div>
-
-        <div className="form-group result-container">
-          <label htmlFor="resultRange">Result Range:</label>
-          <input
-            type="number"
-            id="resultRangeMin"
-            value={settings.resultRange[0]}
-            onChange={(e) => handleChange('resultRange', [parseInt(e.target.value), settings.resultRange[1]])}
-            placeholder="Min"
-          />
-          <input
-            type="number"
-            id="resultRangeMax"
-            value={settings.resultRange[1]}
-            onChange={(e) => handleChange('resultRange', [settings.resultRange[0], parseInt(e.target.value)])}
-            placeholder="Max"
-          />
-        </div>
-
-        <div className="form-group operands-container">
-          <label htmlFor="numOperandsRange">Number of Operands Range:</label>
-          <input
-            type="number"
-            id="numOperandsRangeMin"
-            value={settings.numOperandsRange[0]}
-            onChange={(e) => handleChange('numOperandsRange', [parseInt(e.target.value), settings.numOperandsRange[1]])}
-            placeholder="Min"
-          />
-          <input
-            type="number"
-            id="numOperandsRangeMax"
-            value={settings.numOperandsRange[1]}
-            onChange={(e) => handleChange('numOperandsRange', [settings.numOperandsRange[0], parseInt(e.target.value)])}
-            placeholder="Max"
-          />
-        </div>
-
-        <div className="form-group">
+        <div>
           <label htmlFor="allowNegative">Allow Negative Results:</label>
           <input
             type="checkbox"
@@ -288,49 +190,6 @@ function App() {
             checked={settings.allowNegative}
             onChange={(e) => handleChange('allowNegative', e.target.checked)}
           />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="showAnswers">Show Answers:</label>
-          <input
-            type="checkbox"
-            id="showAnswers"
-            checked={settings.showAnswers}
-            onChange={(e) => handleChange('showAnswers', e.target.checked)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="fontSize">Font Size:</label>
-          <input
-            type="number"
-            id="fontSize"
-            value={settings.fontSize}
-            onChange={(e) => handleChange('fontSize', parseInt(e.target.value))}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="lineSpacing">Line Spacing (px):</label>
-          <input
-            type="number"
-            id="lineSpacing"
-            value={settings.lineSpacing}
-            onChange={(e) => handleChange('lineSpacing', parseInt(e.target.value))}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="paperSize">Paper Size:</label>
-          <select
-            id="paperSize"
-            value={settings.paperSize}
-            onChange={(e) => handleChange('paperSize', e.target.value)}
-          >
-            <option value="a4">A4</option>
-            <option value="letter">Letter</option>
-            <option value="legal">Legal</option>
-          </select>
         </div>
 
         <button onClick={generateProblems}>Generate Problems</button>{' '}
@@ -342,9 +201,12 @@ function App() {
           ))}
         </div>
       </div>
-      <SpeedInsights />
+      <Suspense fallback={<div>Loading insights...</div>}>
+        <SpeedInsights />
+      </Suspense>
     </div>
   );
 }
 
-export default App; // Export the App component for use in other files
+export default App;
+
