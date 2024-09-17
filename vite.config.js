0506ef2a56a5a75f9a3b3748b12e92aed8822923
Kang Liu <555836+cnkang@users.noreply.github.com>
2024-09-17 09:30:16 +0800

    fix build issue

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
     }),
   ],
   build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
     outDir: 'dist',
     rollupOptions: {
       output: {
