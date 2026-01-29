63dccf7fc11b24a9741a1315c5a2728f9fbe0042
Kang <555836+cnkang@users.noreply.github.com>
2026-01-29 17:04:02 +0800

    build(postcss): update plugin config format

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,19 @@
 /* eslint-env node */
 /* global process */
-const { default: purgecss } = require('@fullhuman/postcss-purgecss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  plugins: [
-    isProduction &&
-      purgecss({
-        content: ['./src/**/*.html', './src/**/*.ts', './src/**/*.tsx'],
-        defaultExtractor: content =>
-          (content.match(/[\w-/:%]+/g) || []).filter(token => !token.endsWith(':')),
-      }),
-    autoprefixer,
-    cssnano({
+  plugins: {
+    '@fullhuman/postcss-purgecss': isProduction
+      ? {
+          content: ['./src/**/*.html', './src/**/*.ts', './src/**/*.tsx'],
+          defaultExtractor: content =>
+            (content.match(/[\w-/:%]+/g) || []).filter(token => !token.endsWith(':')),
+        }
+      : false,
+    autoprefixer: {},
+    cssnano: {
       preset: 'advanced',
-    }),
-  ].filter(Boolean),
+    },
+  },
 };
